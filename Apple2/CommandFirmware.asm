679debded41986e783bd1994642e4a9e55aaee71
Terence Boldt <terence@boldt.ca>
2021-03-28 10:36:12 -0400

    Add command line to firmware

--- Apple2/CommandFirmware.asm
+++ Apple2/CommandFirmware.asm
@@ -46,12 +46,50 @@ DriverEntry:
  sta OutputFlags
 
 Start:
-; Put command firmware here
-;
-;
-;
+ lda $33
+ pha
+ lda #$a4
+ sta $33
+GetCommand:
+ jsr $fd6a
+ lda $0200
+ cmp #$8d ;stop when return found
+ beq ExitApp
+ jsr DumpOutput
+ clc
+ bcc GetCommand
+ExitApp:
+ pla
+ sta $33
+ lda #$00
+ sty $34
  rts
 
+DumpOutput:
+ ldx #$50
+ lda #$05 ;send command 5 = exec
+ jsr SendByte
+ ldy #$00
+getInput:
+ lda $0200,y
+ cmp #$8d
+ beq sendNullTerminator
+ and #$7f
+ jsr SendByte
+ iny
+ bne getInput
+sendNullTerminator:
+ lda #$00
+ jsr SendByte
+getOutput:
+ jsr GetByte
+ cmp #$00
+ beq endOutput
+ jsr $fded
+ clc
+ bcc getOutput
+endOutput:
+ rts
 
 SendByte:
  pha 
@@ -98,4 +136,9 @@ end:
 .byte      0,0     ;0000 blocks = check status
 .byte      7       ;bit set(0=status 1=read 2=write) unset(3=format, 4/5=number of volumes, 6=interruptable, 7=removable)
 .byte     DriverEntry&$00FF ;low byte of entry
+
+;Translation of code written in mini-assembler on Apple //e
+;Currently only works if card is in slot 5
+
+ .org $1000
 

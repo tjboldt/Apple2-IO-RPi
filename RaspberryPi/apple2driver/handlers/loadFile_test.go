1419fec389b073773b35e0fb26ae29aec01d367f
Terence Boldt <terence@boldt.ca>
2021-10-30 19:30:05 -0400

    Add test for load file command

--- RaspberryPi/apple2driver/handlers/loadFile_test.go
+++ RaspberryPi/apple2driver/handlers/loadFile_test.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"os"
 	"testing"
 
 	"github.com/tjboldt/Apple2-IO-RPi/RaspberryPi/apple2driver/a2io"
@@ -22,5 +23,33 @@ func TestLoadFileCommandReturnZeroLengthOnFileNotFound(t *testing.T) {
 
 	if got[0] != 0 && got[1] != 0 {
 		t.Errorf("MenuCommand() sent = %s; want 00", got)
+	}
+}
+
+func TestLoadFileCommandReturnsFile(t *testing.T) {
+	mockIoData := a2io.MockIoData{
+		BytesToRead:  []byte("loadFile.go\x00"),
+		BytesWritten: make([]byte, 10000),
+	}
+
+	mockIo := a2io.MockIo{Data: &mockIoData}
+
+	SetCommunication(&mockIo)
+
+	LoadFileCommand()
+
+	fileInfo, _ := os.Stat("loadFile.go")
+
+	got := mockIoData.BytesWritten
+
+	lowSize := fileInfo.Size() & 0xFF
+	hiSize := (fileInfo.Size() & 0xFF00) >> 8
+
+	if got[0] != byte(lowSize) && got[1] != byte(hiSize) {
+		t.Errorf("MenuCommand() fileSize = %02X%02X; want %02X%02X", got[1], got[0], hiSize, lowSize)
+	}
+
+	if mockIoData.NumberBytesWritten-2 != int(fileInfo.Size()) {
+		t.Errorf("MenuCommand() byte count = %04X; want %04X", mockIoData.NumberBytesWritten-2, fileInfo.Size())
 	}
 }

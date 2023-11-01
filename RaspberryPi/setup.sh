9765877ad89a1c12e73936a3096890ee15e360ab
Terence Boldt <terence@boldt.ca>
2023-10-31 21:47:49 -0400

    Add 64 bit OS support, upgrade Go, allow non-pi user

--- RaspberryPi/setup.sh
+++ RaspberryPi/setup.sh
@@ -2,43 +2,66 @@
 
 sudo apt update
 sudo apt install git -y
-wget https://golang.org/dl/go1.17.3.linux-armv6l.tar.gz
-sudo tar -C /usr/local -xzf go1.17.3.linux-armv6l.tar.gz
+if [ "$(uname -m)" = 'aarch64' ]; then
+  if [ ! -f go1.21.3.linux-arm64.tar.gz ]; then  
+    wget https://go.dev/dl/go1.21.3.linux-arm64.tar.gz
+    sudo tar -C /usr/local -xzf go1.21.3.linux-arm64.tar.gz
+  fi
+else
+  if [ ! -f go1.21.3.linux-arm6l.tar.gz ]; then  
+    wget https://golang.org/dl/go1.21.3.linux-armv6l.tar.gz
+    sudo tar -C /usr/local -xzf go1.21.3.linux-armv6l.tar.gz
+  fi
+fi
+if [ -f "/usr/bin/go" ]; then
+    sudo rm /usr/bin/go
+fi
 sudo ln -s /usr/local/go/bin/go /usr/bin/go
-git clone https://github.com/tjboldt/ProDOS-Utilities.git
+if [ ! -d "ProDOS-Utilities" ]; then
+  git clone https://github.com/tjboldt/ProDOS-Utilities.git
+fi
 cd ProDOS-Utilities || exit
 go build
 cd ~ || exit
+if [ -L "/usr/bin/ProDOS-Utilities" ]; then
+    sudo rm /usr/bin/ProDOS-Utilities
+fi
 sudo ln -s /home/pi/ProDOS-Utilities/ProDOS-Utilities /usr/bin/ProDOS-Utilities
-git clone https://github.com/tjboldt/Apple2-IO-RPi.git
+if [ ! -d "Apple2-IO-RPi" ]; then
+  git clone https://github.com/tjboldt/Apple2-IO-RPi.git
+fi
 cd Apple2-IO-RPi/RaspberryPi/apple2driver || exit
 go build
 sudo apt install cc65 vim -y
 cd ~ || exit
+
 sudo bash -c 'cat > /boot/config.txt << EOF
 disable_splash=1
 dtoverlay=disable-bt
 boot_delay=0
 EOF'
 sudo bash -c 'echo " quiet" >> /boot/cmdline.txt'
-sudo bash -c 'cat > /etc/systemd/system/apple2driver.service << EOF
+bash -c 'cat > apple2driver.service << EOF
 [Unit]
 Description=Apple2-IO-RPi Driver
 
 [Service]
-ExecStart=/home/pi/Apple2-IO-RPi/RaspberryPi/apple2driver/apple2driver
+ExecStart=/home/$USER/Apple2-IO-RPi/RaspberryPi/apple2driver/apple2driver
 StandardOutput=syslog
 StandardError=syslog
 SyslogIdentifier=apple2driver
-User=pi
-Group=pi
-WorkingDirectory=/home/pi/Apple2-IO-RPi/RaspberryPi/apple2driver
+User=$USER
+Group=$USER
+WorkingDirectory=/home/$USER/Apple2-IO-RPi/RaspberryPi/apple2driver
 
 [Install]
 WantedBy=basic.target
 EOF'
+sudo mv apple2driver.service /etc/systemd/system/apple2driver.service  
+sudo chown root:root /etc/systemd/system/apple2driver.service  
 sudo systemctl start apple2driver
 sudo systemctl enable apple2driver
 sudo systemctl disable avahi-daemon.service
 sudo systemctl disable triggerhappy.service
 sudo systemctl disable raspi-config.service
+sudo systemctl daemon-reload

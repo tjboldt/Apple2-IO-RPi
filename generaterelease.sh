f8b24f84230641c2fc90abb38d5901e5c3444cab
Terence Boldt <terence@boldt.ca>
2024-05-30 08:44:52 -0400

    Add EEPROM image to release

--- generaterelease.sh
+++ generaterelease.sh
@@ -12,6 +12,7 @@ cd ../RaspberryPi/driveimage
 cd ../../Apple2
 . assemble.sh 0
 cd ../RaspberryPi/driveimage
+cp AT28C64B.bin ../../release
 ../../.cicd/ProDOS-Utilities -d ../../release/Apple2-IO-RPi.classic.hardware.hdv -c create -v APPLE2.IO.RPI
 ../../.cicd/ProDOS-Utilities -d ../../release/Apple2-IO-RPi.classic.hardware.hdv -c putall
 ../../.cicd/ProDOS-Utilities -d ../../release/Apple2-IO-RPi.classic.hardware.hdv -c ls
